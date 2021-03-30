210dff350d2109a214d2f1a818d25bb8fe0f73d4
ventolotl <ventolotl@users.noreply.github.com>
2021-03-30 13:33:52 +0200

    fix errors when teleporting a player if obfuscated

--- src/main/java/de/jpx3/intave/event/dispatch/TeleportPositionFlagsHelper.java
+++ src/main/java/de/jpx3/intave/event/dispatch/TeleportPositionFlagsHelper.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.utility.MinecraftReflection;
 import com.comphenix.protocol.wrappers.EnumWrappers;
+import de.jpx3.intave.tools.annotate.KeepEnumInternalNames;
 
 import java.util.Set;
 
@@ -17,6 +18,7 @@ final class TeleportPositionFlagsHelper {
     return packet.getSets(EnumWrappers.getGenericConverter(FLAGS_CLASS, PlayerTeleportFlag.class));
   }
 
+  @KeepEnumInternalNames
   public enum PlayerTeleportFlag {
     X, Y, Z, Y_ROT, X_ROT
   }
