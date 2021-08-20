a0450dfcc6963ae30b31a32455659a8e35c29cce
DarkAndBlue <13067258+DarkAndBlue@users.noreply.github.com>
2021-08-20 15:08:49 +0200

    Replaced the EntityTypeData of DestroyedWrappedEntity.java so its not null

--- src/main/java/de/jpx3/intave/module/tracker/entity/DestroyedWrappedEntity.java
+++ src/main/java/de/jpx3/intave/module/tracker/entity/DestroyedWrappedEntity.java
@@ -1,8 +1,11 @@
 package de.jpx3.intave.module.tracker.entity;
 
+import de.jpx3.intave.reflect.hitbox.HitBoxBoundaries;
+import de.jpx3.intave.reflect.hitbox.typeaccess.EntityTypeData;
+
 public final class DestroyedWrappedEntity extends WrappedEntity {
   public DestroyedWrappedEntity() {
-    super(0, null, false);
+    super(0, new EntityTypeData("destroyed", HitBoxBoundaries.zero(),-1, false), false);
   }
 
   @Override
