578a4fbe5f2a1e83b13dff0359c7b9a71c22aa12
Lennox <admin@lennoxlotl.dev>
2022-09-17 20:15:17 +0200

    fix formatting

--- src/main/java/de/jpx3/intave/check/combat/heuristics/detect/other/AttackInInvalidStateHeuristic.java
+++ src/main/java/de/jpx3/intave/check/combat/heuristics/detect/other/AttackInInvalidStateHeuristic.java
@@ -28,134 +28,134 @@ import static de.jpx3.intave.module.linker.packet.PacketId.Client.USE_ENTITY;
 import static de.jpx3.intave.user.meta.ProtocolMetadata.VER_1_8;
 
 public final class AttackInInvalidStateHeuristic extends MetaCheckPart<Heuristics, AttackInInvalidStateHeuristic.AttackInInvalidStateMeta> {
-    public AttackInInvalidStateHeuristic(Heuristics heuristics) {
-        super(heuristics, AttackInInvalidStateMeta.class);
-    }
+  public AttackInInvalidStateHeuristic(Heuristics heuristics) {
+    super(heuristics, AttackInInvalidStateMeta.class);
+  }
 
-    @PacketSubscription(
+  @PacketSubscription(
 //    priority = ListenerPriority.HIGH,
-            packetsIn = {
-                    USE_ENTITY
-            }
-    )
-    public void receiveAttack(PacketEvent event) {
-        Player player = event.getPlayer();
-        PacketContainer packet = event.getPacket();
-        User user = userOf(player);
-        ProtocolMetadata clientData = user.meta().protocol();
-        EnumWrappers.EntityUseAction action = packet.getEntityUseActions().readSafely(0);
-        if (action == null) {
-            action = packet.getEnumEntityUseActions().read(0).getAction();
+    packetsIn = {
+      USE_ENTITY
+    }
+  )
+  public void receiveAttack(PacketEvent event) {
+    Player player = event.getPlayer();
+    PacketContainer packet = event.getPacket();
+    User user = userOf(player);
+    ProtocolMetadata clientData = user.meta().protocol();
+    EnumWrappers.EntityUseAction action = packet.getEntityUseActions().readSafely(0);
+    if (action == null) {
+      action = packet.getEnumEntityUseActions().read(0).getAction();
+    }
+    if (action != EnumWrappers.EntityUseAction.ATTACK) {
+      return;
+    }
+    if (clientData.protocolVersion() <= VER_1_8) {
+      checkGUIScreen(player);
+    }
+    checkDeadEntity(player, packet);
+    checkBlocking(event);
+  }
+
+  private void checkBlocking(PacketEvent event) {
+    Player player = event.getPlayer();
+    User user = userOf(player);
+
+    // On 1.9+ there is an error when switching to a consumable item for 1 tick
+    if (user.protocolVersion() > 47) {
+      ItemStack heldItem = user.meta().inventory().heldItem();
+      if (heldItem != null) {
+        // Swords are not consumable on 1.9+
+        boolean sword = ItemProperties.isSwordItem(heldItem);
+        boolean heldItemConsumable = ItemProperties.canItemBeUsed(player, heldItem) && !sword;
+        if (user.meta().inventory().pastHotBarSlotChange < 2 && heldItemConsumable) {
+          return;
         }
-        if (action != EnumWrappers.EntityUseAction.ATTACK) {
-            return;
-        }
-        if (clientData.protocolVersion() <= VER_1_8) {
-            checkGUIScreen(player);
-        }
-        checkDeadEntity(player, packet);
-        checkBlocking(event);
+      }
     }
 
-    private void checkBlocking(PacketEvent event) {
-        Player player = event.getPlayer();
-        User user = userOf(player);
-
-        // On 1.9+ there is an error when switching to a consumable item for 1 tick
-        if (user.protocolVersion() > 47) {
-            ItemStack heldItem = user.meta().inventory().heldItem();
-            if (heldItem != null) {
-                // Swords are not consumable on 1.9+
-                boolean sword = ItemProperties.isSwordItem(heldItem);
-                boolean heldItemConsumable = ItemProperties.canItemBeUsed(player, heldItem) && !sword;
-                if (user.meta().inventory().pastHotBarSlotChange < 2 && heldItemConsumable) {
-                    return;
-                }
-            }
-        }
-
-        // not checked yet
-        if (user.meta().inventory().handActive()) {
-            player.sendMessage("active: " + user.meta().inventory().heldItemType());
-            Anomaly anomaly = Anomaly.anomalyOf("162", Confidence.NONE, Anomaly.Type.KILLAURA, "attacked whilst using an item");
-            parentCheck().saveAnomaly(player, anomaly);
-            //dmc28
-            user.applyAttackNerfer(AttackNerfStrategy.BLOCKING, "28");
+    // not checked yet
+    if (user.meta().inventory().handActive()) {
+      player.sendMessage("active: " + user.meta().inventory().heldItemType());
+      Anomaly anomaly = Anomaly.anomalyOf("162", Confidence.NONE, Anomaly.Type.KILLAURA, "attacked whilst using an item");
+      parentCheck().saveAnomaly(player, anomaly);
+      //dmc28
+      user.applyAttackNerfer(AttackNerfStrategy.BLOCKING, "28");
 //      user.applyAttackNerfer(AttackNerfStrategy.DMG_LIGHT, "28");
 
-            sendStopUseItemPacketToServer(user);
-        }
+      sendStopUseItemPacketToServer(user);
     }
+  }
 
-    private void sendStopUseItemPacketToServer(User user) {
-        Player player = user.player();
-        if (ProtocolLibraryAdapter.serverVersion().isAtLeast(MinecraftVersions.VER1_9_0)) {
-            return;
-        } else {
-            PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Client.BLOCK_DIG);
-            packet.getBlockPositionModifier().write(0, new BlockPosition(0, 0, 0));
-            packet.getDirections().write(0, EnumWrappers.Direction.DOWN);
-            packet.getPlayerDigTypes().write(0, EnumWrappers.PlayerDigType.RELEASE_USE_ITEM);
+  private void sendStopUseItemPacketToServer(User user) {
+    Player player = user.player();
+    if (ProtocolLibraryAdapter.serverVersion().isAtLeast(MinecraftVersions.VER1_9_0)) {
+      return;
+    } else {
+      PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Client.BLOCK_DIG);
+      packet.getBlockPositionModifier().write(0, new BlockPosition(0, 0, 0));
+      packet.getDirections().write(0, EnumWrappers.Direction.DOWN);
+      packet.getPlayerDigTypes().write(0, EnumWrappers.PlayerDigType.RELEASE_USE_ITEM);
 
-            userOf(player).ignoreNextInboundPacket();
-            PacketSender.receiveClientPacketFrom(player, packet);
+      userOf(player).ignoreNextInboundPacket();
+      PacketSender.receiveClientPacketFrom(player, packet);
 //        ProtocolLibrary.getProtocolManager().recieveClientPacket(player, packet, true);
 
-            updatePlayerHandItem(player);
-        }
-        Synchronizer.synchronize(player::updateInventory);
+      updatePlayerHandItem(player);
     }
+    Synchronizer.synchronize(player::updateInventory);
+  }
 
-    private void updatePlayerHandItem(Player player) {
-        User user = UserRepository.userOf(player);
-        InventoryMetadata inventoryData = user.meta().inventory();
-        inventoryData.deactivateHand();
-    }
+  private void updatePlayerHandItem(Player player) {
+    User user = UserRepository.userOf(player);
+    InventoryMetadata inventoryData = user.meta().inventory();
+    inventoryData.deactivateHand();
+  }
 
-    private void checkGUIScreen(Player player) {
-        User user = userOf(player);
-        AttackInInvalidStateMeta meta = metaOf(user);
-        ProtocolMetadata clientData = user.meta().protocol();
-        AbilityMetadata abilityData = user.meta().abilities();
-        float health = abilityData.health;
-        if (health <= 0f) {
-            long now = System.currentTimeMillis();
-            long lastFlag = now - meta.lastGUIAttackTimestamps;
-            int ticksAgo = abilityData.ticksToLastHealthUpdate;
-            Confidence confidence = lastFlag > 1000 ? Confidence.PROBABLE : Confidence.NONE;
-            String description = "attacked in gui screen (version " + clientData.versionString() + ") | ";
-            description += "lastHealthUpdate: " + ticksAgo + ", ";
-            description += "lastFlag " + lastFlag + " ms ago, ";
-            description += "confidence " + confidence.level();
-            Anomaly anomaly = Anomaly.anomalyOf("161", confidence, Anomaly.Type.KILLAURA, description);
-            parentCheck().saveAnomaly(player, anomaly);
-            meta.lastGUIAttackTimestamps = now;
-        }
+  private void checkGUIScreen(Player player) {
+    User user = userOf(player);
+    AttackInInvalidStateMeta meta = metaOf(user);
+    ProtocolMetadata clientData = user.meta().protocol();
+    AbilityMetadata abilityData = user.meta().abilities();
+    float health = abilityData.health;
+    if (health <= 0f) {
+      long now = System.currentTimeMillis();
+      long lastFlag = now - meta.lastGUIAttackTimestamps;
+      int ticksAgo = abilityData.ticksToLastHealthUpdate;
+      Confidence confidence = lastFlag > 1000 ? Confidence.PROBABLE : Confidence.NONE;
+      String description = "attacked in gui screen (version " + clientData.versionString() + ") | ";
+      description += "lastHealthUpdate: " + ticksAgo + ", ";
+      description += "lastFlag " + lastFlag + " ms ago, ";
+      description += "confidence " + confidence.level();
+      Anomaly anomaly = Anomaly.anomalyOf("161", confidence, Anomaly.Type.KILLAURA, description);
+      parentCheck().saveAnomaly(player, anomaly);
+      meta.lastGUIAttackTimestamps = now;
     }
+  }
 
-    private void checkDeadEntity(Player player, PacketContainer packet) {
-        User user = userOf(player);
-        AttackMetadata attackData = user.meta().attack();
-        ProtocolMetadata clientData = user.meta().protocol();
-        EntityShade entity = attackData.lastAttackedEntity();
-        if (entity == null || !entity.clientSynchronized || !entity.typeData().isLivingEntity()) {
-            return;
-        }
-        if (clientData.protocolVersion() != VER_1_8) {
-            return;
-        }
-        EnumWrappers.EntityUseAction action = packet.getEntityUseActions().readSafely(0);
-        if (action == null) {
-            action = packet.getEnumEntityUseActions().read(0).getAction();
-        }
-        if (action == EnumWrappers.EntityUseAction.ATTACK && entity.dead) {
-            String description = "attacked a dead entity " + entity.entityName();
-            Anomaly anomaly = Anomaly.anomalyOf("161", Confidence.NONE, Anomaly.Type.KILLAURA, description);
-            parentCheck().saveAnomaly(player, anomaly);
-        }
+  private void checkDeadEntity(Player player, PacketContainer packet) {
+    User user = userOf(player);
+    AttackMetadata attackData = user.meta().attack();
+    ProtocolMetadata clientData = user.meta().protocol();
+    EntityShade entity = attackData.lastAttackedEntity();
+    if (entity == null || !entity.clientSynchronized || !entity.typeData().isLivingEntity()) {
+      return;
     }
+    if (clientData.protocolVersion() != VER_1_8) {
+      return;
+    }
+    EnumWrappers.EntityUseAction action = packet.getEntityUseActions().readSafely(0);
+    if (action == null) {
+      action = packet.getEnumEntityUseActions().read(0).getAction();
+    }
+    if (action == EnumWrappers.EntityUseAction.ATTACK && entity.dead) {
+      String description = "attacked a dead entity " + entity.entityName();
+      Anomaly anomaly = Anomaly.anomalyOf("161", Confidence.NONE, Anomaly.Type.KILLAURA, description);
+      parentCheck().saveAnomaly(player, anomaly);
+    }
+  }
 
-    public static final class AttackInInvalidStateMeta extends CheckCustomMetadata {
-        public long lastGUIAttackTimestamps;
-    }
+  public static final class AttackInInvalidStateMeta extends CheckCustomMetadata {
+    public long lastGUIAttackTimestamps;
+  }
 }
